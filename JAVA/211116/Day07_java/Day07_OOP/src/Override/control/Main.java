96221f5ecc53cace6c37455638755bd936a5b7a9
muse9312 <muse9312@naver.com>
2021-11-16 17:49:39 +0900

    Day07_java : Override modify

--- JAVA/211116/Day07_java/Day07_OOP/src/Override/control/Main.java
+++ JAVA/211116/Day07_java/Day07_OOP/src/Override/control/Main.java
@@ -1,5 +1,11 @@
 package Override.control;
 
+import Override.entitiy.MyFrame;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        MyFrame f = new MyFrame();
+
+    }
 
 }
--- JAVA/211116/Day07_java/Day07_OOP/src/Override/entitiy/MyFrame.java
+++ JAVA/211116/Day07_java/Day07_OOP/src/Override/entitiy/MyFrame.java
@@ -1,10 +1,36 @@
 package Override.entitiy;
 
 import javax.swing.JFrame;
+import java.awt.*;
+import java.util.Random;
 
 public class MyFrame extends JFrame {
+    int Frame_width = 800;
+    int Frame_height = 600;
 
     public MyFrame() {
+        super(); // JFrame f = new JFrame();
+        this.setSize(Frame_width, Frame_height);
+        this.setVisible(true);
+
     }
 
+    @Override
+    public void paint(Graphics g) {
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+
+            int x = rand.nextInt(800);
+            int y = rand.nextInt(600);
+
+            int red = rand.nextInt(256);
+            int green = rand.nextInt(256);
+            int blue = rand.nextInt(256);
+            Color c = new Color(red, green, blue);
+            g.setColor(c);
+            g.fillOval(x, y, 30, 30);
+
+        }
+
+    }
 }
