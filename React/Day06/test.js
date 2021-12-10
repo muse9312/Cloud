319e501e6edffce721360439b96f389b5504993a
muse9312 <muse9312@naver.com>
2021-12-10 12:05:42 +0900

    Add test.js

---
+++ React/Day06/test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function test() {
+
+    const arr = ['a', 'b', 'c']
+
+    console.log('리액트에서 자주 사용되는 방식');
+    arr.map(function (value) {
+        console.log(value);
+    })
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
